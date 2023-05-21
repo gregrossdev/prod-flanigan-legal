03e00619e42ce1ac6da419a101309b7a65befed0
Gregory Ross <gregross.dev@gmail.com>
2023-05-21 18:39:19 -0400

    web routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/{practice:slug}', [\App\Http\Controllers\PracticeController::class, 'show']);
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/practices', function () {
+    return view('practices');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
